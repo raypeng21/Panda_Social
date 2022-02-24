01b8b1eead6a98c9c0276447a6939f0cf06b8065
RP Zenbook <peng-ray@hotmail.com>
2022-02-24 00:30:40 -0600

    register ref value post to mongodb

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -29,6 +29,8 @@ router.post("/register", async (req,res) => {
         res.status(200).json(user);
     }catch(err){
         res.status(500).json(err)
+
+        console.log(err)
     }
 });
 
--- front-end/src/components/Register/Register.jsx
+++ front-end/src/components/Register/Register.jsx
@@ -1,18 +1,57 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import './register.scss';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import axios from "axios";
+
 function Register() {
+
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordAgain = useRef();
+  const histroy = useHistory();
+
+
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    if(passwordAgain.current.value  !== password.current.value){
+      passwordAgain.current.setCustomValidity("Passwords don't match")
+    }else{
+      const user = {
+        username: username.current.value,
+        email: email.current.value,
+        password: password.current.value
+      }
+      try{
+        await axios.post("/auth/register", user)
+        histroy.push("/signin")
+      }catch(err){
+        console.log(err)
+      }
+    }
+
+  };
+
+
   return (
     <div className='register' >
         <div className="register_Container">
             <img src="Assets/Panda_Social_logo.png" alt="" />
 
-            <input type="text" placeholder='Username'/>
-            <input type="email" placeholder='Email'/>
-            <input type="password" placeholder='Password'/>
-            <input type="password_again" placeholder='Password Again'/>
 
-            <button className='home_register'>Register a new account</button>
+            <form className='register_box' onSubmit={handleClick}>
+              <input type="text" required ref = {username} placeholder='Username'/>
+              <input type="email" required ref = {email} placeholder='Email'/>
+              <input type="password" required ref = {password} minLength ="6" placeholder='Password'/>
+              <input type="password" required ref = {passwordAgain} placeholder='Password Again'/>
+
+              <button type = "submit" className='home_register'>Register a new account</button>
+
+
+            </form>
+
 
             <Link to = '/signin'>
             <p className='register_back'>Back to Sign in</p>
